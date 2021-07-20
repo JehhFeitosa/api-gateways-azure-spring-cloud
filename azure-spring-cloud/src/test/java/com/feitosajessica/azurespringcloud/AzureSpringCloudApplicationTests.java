3ec27cfc0914a320050c58c6e325e3041b1d412e
JehhFeitosa <jessicaadrianafeitosa@hotmail.com>
2021-07-20 00:13:50 -0300

    Spring Boot no container

---
+++ azure-spring-cloud/src/test/java/com/feitosajessica/azurespringcloud/AzureSpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feitosajessica.azurespringcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureSpringCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
