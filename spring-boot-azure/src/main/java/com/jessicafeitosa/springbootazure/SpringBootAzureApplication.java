00e8b9539843abc133df1234c8c4a6475718331f
JehhFeitosa <jessicaadrianafeitosa@hotmail.com>
2021-07-19 22:40:32 -0300

    Configuração e autenticação Azure

--- spring-boot-azure/src/main/java/com/jessicafeitosa/springbootazure/SpringBootAzureApplication.java
+++ spring-boot-azure/src/main/java/com/jessicafeitosa/springbootazure/SpringBootAzureApplication.java
@@ -2,7 +2,12 @@ package com.jessicafeitosa.springbootazure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @SpringBootApplication
 public class SpringBootAzureApplication {
 
